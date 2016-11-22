969dd87d194d2ddfbe3d3fcbb52be40b4311ac14
周超 <zhouchao@zhixueyun.com>
2016-11-22 16:04:55 +0800

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  port: 22222,
+  uploadDir: 'D:/diamond/front/file-upload-node/public/',
+  downDomain: 'http://localhost:22222/file/'
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs = require('fs');
+var multiparty = require('multiparty');
+var config = require(process.argv[2] || './config')
+var app = express();
+var uuid = require('node-uuid').v4;
+
+app.post('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers['origin']);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", req.headers['access-control-request-headers']);
+    res.header("Access-Control-Allow-Methods",req.headers['access-control-request-method']);
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+app.get('/file/:file', function(req, res) {
+    var filename = req.params.file
+    var filepath = config.uploadDir + filename;
+    res.header("Content-Type", "image/png");
+    res.sendFile(config.uploadDir + filename);
+});
+
+app.post('/upload', function (req, res){
+
+    var form = new multiparty.Form({uploadDir: config.uploadDir});
+    form.on('error', function(err) {
+        console.log('Error parsing form: ' + err.stack);
+        res.send({success: false, msg: err.toString()})
+    });
+    form.parse(req, function (err, fields, files){
+        if (err){
+            res.send({success: false, msg: err.toString()});
+        } else {
+            var fileDataArr = files['file'];
+            var fileData = fileDataArr[0];
+            var oldName = fileData.path;
+            var newName = uuid() + oldName.substr(oldName.lastIndexOf('.'))
+            fs.rename(oldName, config.uploadDir + newName)
+            res.send(config.downDomain + newName);
+        }
+    });
+});
+
+app.listen(config.port);
+console.log('listen on port:', config)
